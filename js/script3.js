1c9e97963966dd4022b232efa9f8b7f69900007b
Cristina <crispv9@hotmail.com>
2020-02-06 10:17:25 +0100

    .

--- js/script3.js
+++ js/script3.js
@@ -1,8 +1,17 @@
 
 
 $(document).ready(function() {
+//autoplay
 
-  $( window ).load(function() {
-    $("#videointroductorio").play();
-  });
+  //audio
+   $("#videointroductorio").prop('muted', true);
+
+     $("#videointroductorio").click( function (){
+         $(this).prop('muted', !$(this).prop('muted'));
+     });
+
+     $("#audio").click( function (){
+         $("#videointroductorio").prop('muted', !$("#videointroductorio").prop('muted'));
+   });
+
 });
